5d8031e30110951b8ad03607a63b01152b615c29
You-hyeonyeong <dbgusdud412@naver.com>
2019-10-22 17:22:33 +0900

    상품상세조회 완료

--- 3rd/market/routes/mypage/mypage.js
+++ 3rd/market/routes/mypage/mypage.js
@@ -4,15 +4,47 @@ const db = require('../../modules/pool');
 var utils = require('../../modules/utils');
 
 //마이페이지 조회
+// name vilage id 구매내역 판매내역, 좋아요상품
 router.get('/:userIdx', async (req, res) => {
     const userIdx = req.params.userIdx;
-    const getUser = 'SELECT * FROM user WHERE userIdx = ?'
+    const getUser = 'SELECT u.userName, u.userId, v.vilage FROM user u, vilage v WHERE u.vilageIdx = v.vilageIdx AND userIdx = ?'
+    const getAccount = 'SELECT p.name p.info p.price a.type FROM product p, account a WHERE p.productIdx = a.productIdx AND p.userIdx = ?'
     const getUserResult = await db.queryParam_Arr(getUser,[userIdx]);
+    const getAccountResult = await db.queryParam_Arr(getAccount,[userIdx]);
 
     if(!getUserResult) {
         res.status(200).send(utils.successFalse("마이페이지 조회 실패"));
     } else {
-        res.status(200).send(utils.successTrue("마이페이지 조회 성공", getUserResult));
+        res.status(200).send(utils.successTrue("마이페이지 조회 성공", getUserResult ,getAccountResult));
+    }
+
+});
+
+//판매내역 조회
+router.get('/:userIdx/purchase', async (req, res) => {
+    const userIdx = req.params.userIdx;
+    const account = "PURCHASE";
+    const getPurchase = 'SELECT p.name, p.info, p.price, a.type FROM product p, account a WHERE p.productIdx = a.productIdx AND a.userIdx = ? AND a.type = ?'
+    const getAccountPurchase = await db.queryParam_Arr(getPurchase,[userIdx,account]);
+
+    if(!getAccountPurchase) {
+        res.status(200).send(utils.successFalse("판매내역 조회 실패"));
+    } else {
+        res.status(200).send(utils.successTrue("판매내역 조회 성공", getAccountPurchase));
+    }
+
+});
+//구매내역 조회
+router.get('/:userIdx/buy', async (req, res) => {
+    const userIdx = req.params.userIdx;
+    const account = "BUY";
+    const getBuy = 'SELECT p.name, p.info, p.price, a.type FROM market.product p, market.account a WHERE p.productIdx = a.productIdx AND a.userIdx = ? AND a.type = ?'
+    const getAccountBuy = await db.queryParam_Arr(getBuy,[userIdx,account]);
+
+    if(!getAccountBuy) {
+        res.status(200).send(utils.successFalse("구매내역 조회 실패"));
+    } else {
+        res.status(200).send(utils.successTrue("구매내역 조회 성공", getAccountBuy));
     }
 
 });
--- 3rd/market/routes/product/product.js
+++ 3rd/market/routes/product/product.js
@@ -3,20 +3,51 @@ var router = express.Router();
 const db = require('../../modules/pool');
 var utils = require('../../modules/utils');
 
-//name info price isSold category
-
-
-//상품리스트 조회
+//상품리스트 조회 (최신 업로드 순)
+//name price isSold 업로드 시각
 router.get('/', async (req, res) => {
-    const getProductQuery = 'SELECT * FROM market.product';
+    const getProductQuery = 
+    "SELECT p.productIdx, p.name, p.price, p.isSold, v.vilage,\
+    (SELECT COUNT(*) FROM market.like l WHERE p.productIdx = l.productIdx) as likeCnt,\
+    CASE WHEN TIMESTAMPDIFF(MINUTE, createdAt, CURRENT_TIMESTAMP) < 60 \
+    then CONCAT('끌올 ',TIMESTAMPDIFF(MINUTE, createdAt, CURRENT_TIMESTAMP), ' 분 전') \
+    WHEN TIMESTAMPDIFF(HOUR, createdAt, CURRENT_TIMESTAMP) < 24 \
+            then CONCAT('끌올 ',TIMESTAMPDIFF(HOUR, createdAt, CURRENT_TIMESTAMP), ' 시간 전') \
+   WHEN TIMESTAMPDIFF(DAY, createdAt, CURRENT_TIMESTAMP) < 7 \
+            then CONCAT(TIMESTAMPDIFF(DAY, createdAt, CURRENT_TIMESTAMP), ' 일 전')\
+   WHEN TIMESTAMPDIFF(WEEK, createdAt, CURRENT_TIMESTAMP) < 4\
+            then CONCAT(TIMESTAMPDIFF(WEEK, createdAt, CURRENT_TIMESTAMP), ' 주 전')\
+    else CONCAT(TIMESTAMPDIFF(MONTH, createdAt, CURRENT_TIMESTAMP), ' 달 전')\
+      END as time_stamp \
+    FROM market.product p ,market.category c, market.vilage v \
+    WHERE c.categoryIdx = p.categoryIdx AND p.vilageIdx = v.vilageIdx ORDER BY p.createdAt DESC";
+
     const getAllCategoryResult = await db.queryParam_None(getProductQuery);
 
     if (!getAllCategoryResult) {
-        res.status(200).send(utils.successFalse("상품 조회 실패"));
+        res.status(200).send(utils.successFalse(600,"상품 조회 실패"));
     } else {
-        res.status(200).send(utils.successTrue("상품 조회 성공", getAllCategoryResult));
+        res.status(200).send(utils.successTrue(200,"상품 조회 성공", getAllCategoryResult));
     }
 });
+//상품 상세 조회 
+//name info price isSold category 댓글 이회원의 상품
+router.get('/:productIdx', async (req, res) => {
+    const productIdx = req.params.productIdx;
+    const getProduct = 'SELECT p.name, p.info, c.title, v.vilage \
+    FROM market.product p, market.user u, market.vilage v, market.category c \
+    WHERE p.userIdx = u.userIdx AND p.vilageIdx = v.vilageIdx AND c.categoryIdx = p.categoryIdx AND p.productIdx = ?'
+
+    const getProductResult = await db.queryParam_Arr(getProduct,[productIdx]);
+    console.log(getProductResult)
+    if (!getProductResult) {
+        res.status(200).send(utils.successFalse(600,"상품 상세조회 실패"));
+    } else {
+        if(getProductResult.length == 0) res.status(200).send(utils.successFalse(400,"해당상품 없음"));
+        res.status(200).send(utils.successTrue(200,"상품 상세조회 성공", getProductResult[0]));
+    }
+});
+
 
 //상품등록
 router.post('/:categoryIdx', async (req, res) => {
